c0c7893e1bc34f69ee968ae76275820e48f01ecb
seo22hs <lsc584727@gmail.com>
2026-02-09 14:29:52 +0900

    PULLUP code

---
+++ PULLUP/PULLUP.ino
@@ -0,0 +1,23 @@
+const int ledPin = 13;
+const int buttonPin = 2;
+
+int buttonState = 0;
+
+void setup() {
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+    delay(1000);
+  }
+}
